cc55f88ea080fa74e51c282747ae465293a66916
horz.d <he1236@ajou.ac.kr>
2022-09-19 21:01:55 +0900

    lab3 완성

--- lab/lab3/ShopSupp.java
+++ lab/lab3/ShopSupp.java
@@ -7,52 +7,86 @@ public class ShopSupp {
     private int[] idArr = {0,1,2,3,4};
     private int[] stockArr = {25,25,25,25,25};
     private int[] priceArr = {500,1000,600,1200,1500};
-    private int[] soldArr = {0,2,3,0,0};
     private int[] minStockArr = {1,1,1,1,1};
 
+    // sold info
+    private int[] soldArr = {0,0,0,0,0};
+    private int[] soldPriceArr = {0,0,0,0,0};
+
     public void record(int pId, int soldNum){
-        if ( minStockArr[pId] < stockArr[pId] - soldNum){
-            soldArr[pId] += soldNum;
+        soldArr[pId] += soldNum;
+        soldPriceArr[pId] += soldNum * priceArr[pId];
+        System.out.println("* Successfully recorded on sold-Info system: { product Id:"+ pId + " | Cumulative sales of the item :" + soldPriceArr[pId] + " }");
+    }
+    public void delete(int pId, int soldNum){
+        if ( minStockArr[pId] <= stockArr[pId] - soldNum){
+            this.record(pId, soldNum);
             stockArr[pId] -= soldNum;
 
-            System.out.println("Successfully recorded:"+" { "+ "product Id: " + pId + " | sold Quantity: " + soldNum + " | current stock: " + stockArr[pId] + " } ");
+            System.out.println("* Successfully subtracted:"+" { "+ "product Id: " + pId + " | sold Quantity: " + soldNum + " | current stock: " + stockArr[pId] + " } ");
+
         }
         else {
-            System.out.println("There are only ( " + stockArr[pId] + " items ) you can sell");
+            System.out.println("The product is out of stock");
         }
-        
-    }
-    public void delete(){
 
     }
     
-    public void viewUnitPrice() {
-        for(int p: priceArr)
-            System.out.println(p);
+    public void viewUnitPrice(int pId) {
+        System.out.println("Unit price: " + priceArr[pId]);
     }
 
     public void calculateTotalPrice() {
         int total = 0;
         for(int id: idArr)
             total += soldArr[id] * priceArr[id];
-        System.out.println(total);
+        System.out.println("Total Price: " + total);
+        System.exit(1);
     }
 
     public static void main(String[] args) {
         int inId;
         int inNum;
+        int menuNum;
+
         Scanner scan = new Scanner(System.in);
-
-        System.out.println("Insert product Id: ");
-        inId = scan.nextInt();
-
-        System.out.println("Insert sold qunatity: ");
-        inNum = scan.nextInt();
-        
         ShopSupp supporter = new ShopSupp();
 
-        supporter.record(inId,inNum);
-        // supporter.viewUnitPrice();
-        // supporter.calculateTotalPrice();
+        while(true) {
+            System.out.println("Enter the number of menu you want\n1. Record\n2. ViewUnitPrice\n3. CalculateTotalPrice");
+
+            
+            menuNum = scan.nextInt();
+
+            switch(menuNum) {
+                case 1: 
+                    System.out.println("Insert product Id: ");
+                    inId = scan.nextInt();
+            
+                    System.out.println("Insert sold qunatity: ");
+                    inNum = scan.nextInt();
+                    
+                    
+                    supporter.delete(inId,inNum);
+                    break;
+                
+                case 2:
+                    System.out.println("Which item's price do you want to see?: ");
+                    inId = scan.nextInt();
+                    supporter.viewUnitPrice(inId);
+                    break;
+
+                case 3:
+                    supporter.calculateTotalPrice();
+                    break;
+                default:
+                    System.out.println("Error: Insert the right number");
+                    break;
+
+            }
+            System.out.println("\n");
+            
+        }
+        
     }
 }
